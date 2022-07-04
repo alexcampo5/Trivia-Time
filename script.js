f44877a123bb023d9e151b83acc2a06c91d69c75
Alex Campo <abrettc@gmail.com>
2022-07-04 09:44:19 -0500

    minor bug fixes on html files, JS functionality built out to select the right difficulty and category every time.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('working')
+//THIS IS THE API URL TEMPLATE: https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple
 //This is how you check if a box is checked: document.getElementById("checkbox-id").checked = true;
 
 //Gathering all of the necessary variables from the DOM:
@@ -11,3 +11,39 @@ let gkBox = document.querySelector('#gk-checkbox')
 let sportsBox = document.querySelector('#sports-checkbox')
 let geoBox = document.querySelector('#geo-checkbox')
 let questionArea = document.querySelector('.question-list')
+let startButton = document.querySelector('.start-button')
+
+//function to determine difficulty selected
+
+let getDifficulty = () => {
+  if (easyBox.checked == true) {
+    return 'easy'
+  } else if (mediumBox.checked == true) {
+    return 'medium'
+  } else if (hardBox.checked == true) {
+    return 'hard'
+  }
+}
+
+//function that determines category
+
+let getCategory = () => {
+  if (sportsBox.checked == true) {
+    return '21'
+  } else if (gkBox.checked == true) {
+    return '9'
+  } else if (geoBox.checked == true) {
+    return '22'
+  }
+}
+
+//This makes the appropriate API call for difficulty and topic
+startButton.onclick = async function () {
+  let difficultyOption = getDifficulty()
+  let category = getCategory()
+  let response = await axios.get(
+    `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficultyOption}&type=multiple`
+  )
+  let questionsArray = response.data.results
+  console.log(questionsArray)
+}
