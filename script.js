a4431b8c05b0c6754bd23c7a9da812c98e155525
Alex Campo <abrettc@gmail.com>
2022-07-04 09:12:35 -0500

    HTML elements added, very basic CSS added to home page. Initial DOM selectors in place in JS.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('working')
+//This is how you check if a box is checked: document.getElementById("checkbox-id").checked = true;
+
+//Gathering all of the necessary variables from the DOM:
+
+let checkboxes = document.querySelectorAll('.checkbox')
+let easyBox = document.querySelector('#easy-checkbox')
+let mediumBox = document.querySelector('#medium-checkbox')
+let hardBox = document.querySelector('#hard-checkbox')
+let gkBox = document.querySelector('#gk-checkbox')
+let sportsBox = document.querySelector('#sports-checkbox')
+let geoBox = document.querySelector('#geo-checkbox')
+let questionArea = document.querySelector('.question-list')
